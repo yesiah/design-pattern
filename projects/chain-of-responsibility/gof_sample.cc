19b6558cd98a1843cec3e76ba76b5a1adc8a4dbc
yesiah <hyenhsiang@gmail.com>
2021-06-29 13:29:01 +0800

    Refactor to align with real-world use case

--- projects/chain-of-responsibility/gof_sample.cc
+++ projects/chain-of-responsibility/gof_sample.cc
@@ -1,112 +1,127 @@
 #include <iostream>
 #include <memory>
 
-typedef int Topic;
-constexpr Topic NO_HELP_TOPIC = -1;
-constexpr Topic PRINT_TOPIC = 1;
-constexpr Topic PAPER_ORIENTATION_TOPIC = 2;
-constexpr Topic APPLICATION_TOPIC = 3;
-
-class HelpHandler {
-private:
-    HelpHandler* successor;
-    Topic topic;
-public:
-    HelpHandler(HelpHandler* = nullptr, Topic = NO_HELP_TOPIC);
-
-    virtual bool HasHelp();
-    virtual void SetHandler(HelpHandler*, Topic);
-    virtual void HandleHelp();
+enum class Event {
+    kNoEvent,
+    kPrint,
+    kShowHelp,
 };
 
-HelpHandler::HelpHandler(HelpHandler* h, Topic t) : successor(h), topic(t) {
+class EventHandler {
+private:
+    EventHandler* successor;
+    Event event;
+
+public:
+    EventHandler(EventHandler* = nullptr, Event = Event::kNoEvent);
+
+    virtual bool CanHandle(Event);
+    virtual void SetHandler(EventHandler*, Event);
+    virtual void HandleEvent(Event);
+};
+
+EventHandler::EventHandler(EventHandler* h, Event t) : successor(h), event(t) {
 }
 
-void HelpHandler::SetHandler(HelpHandler* h, Topic t) {
+void EventHandler::SetHandler(EventHandler* h, Event t) {
     successor = h;
-    topic = t;
+    event = t;
 }
 
-bool HelpHandler::HasHelp() {
-    return topic != NO_HELP_TOPIC;
+bool EventHandler::CanHandle(Event evt) {
+    return event == evt;
 }
 
-void HelpHandler::HandleHelp() {
+void EventHandler::HandleEvent(Event evt) {
     if (successor != nullptr) {
-        successor->HandleHelp();
+        successor->HandleEvent(evt);
+    } else {
+        std::cout << "No more successor, event is left unhandled" << std::endl;
     }
 }
 
-class Widget : public HelpHandler {
-private:
-    Widget* parent;
+class Widget : public EventHandler {
 protected:
-    Widget(Widget*, Topic = NO_HELP_TOPIC);
+    Widget(Widget*, Event = Event::kNoEvent);
 };
 
-Widget::Widget(Widget* parent, Topic t) : HelpHandler(parent, t) {
-    this->parent = parent;
+Widget::Widget(Widget* parent, Event t) : EventHandler(parent, t) {
 }
 
 class Button : public Widget {
 public:
-    Button(Widget*, Topic = NO_HELP_TOPIC);
-    virtual void HandleHelp() override;
+    Button(Widget*, Event = Event::kNoEvent);
+    virtual void HandleEvent(Event) override;
 };
 
-Button::Button(Widget* h, Topic t) : Widget(h, t) {
+Button::Button(Widget* h, Event t) : Widget(h, t) {
 }
 
-void Button::HandleHelp() {
-    if (HasHelp()) {
-        std::cout << "Help on Button" << std::endl;
+void Button::HandleEvent(Event evt) {
+    if (CanHandle(evt)) {
+        std::cout << "Handled by Button" << std::endl;
     } else {
-        HelpHandler::HandleHelp();
+        EventHandler::HandleEvent(evt);
     }
 }
 
 class Dialog : public Widget {
 public:
-    Dialog(HelpHandler*, Topic = NO_HELP_TOPIC);
-    virtual void HandleHelp() override;
+    Dialog(EventHandler*, Event = Event::kNoEvent);
+    virtual void HandleEvent(Event) override;
 };
 
-Dialog::Dialog(HelpHandler* h, Topic t) : Widget(nullptr) {
+Dialog::Dialog(EventHandler* h, Event t) : Widget(nullptr) {
     SetHandler(h, t);
 }
 
-void Dialog::HandleHelp() {
-    if (HasHelp()) {
-        std::cout << "Help on the Dialog" << std::endl;
+void Dialog::HandleEvent(Event evt) {
+    if (CanHandle(evt)) {
+        std::cout << "Handled by Dialog" << std::endl;
     } else {
-        HelpHandler::HandleHelp();
+        EventHandler::HandleEvent(evt);
     }
 }
 
-class Application : public HelpHandler {
+class Application : public EventHandler {
 public:
-    Application(Topic t);
-    virtual void HandleHelp() override;
+    Application(Event t);
+    virtual void HandleEvent(Event) override;
 };
 
-Application::Application(Topic t) : HelpHandler(nullptr, t) {
+Application::Application(Event t) : EventHandler(nullptr, t) {
 }
 
-void Application::HandleHelp() {
-    std::cout << "Help on the Application" << std::endl;
+void Application::HandleEvent(Event evt) {
+    if (CanHandle(evt)) {
+        std::cout << "Handled by Application" << std::endl;
+    } else {
+        EventHandler::HandleEvent(evt);
+    }
 }
 
 int main() {
-    auto app = std::make_unique<Application>(APPLICATION_TOPIC);
-    auto dialog = std::make_unique<Dialog>(app.get(), PRINT_TOPIC);
-    auto button = std::make_unique<Button>(dialog.get(), PAPER_ORIENTATION_TOPIC);
+    auto app = std::make_unique<Application>(Event::kPrint);
+    auto dialog = std::make_unique<Dialog>(app.get(), Event::kShowHelp);
+    auto button = std::make_unique<Button>(dialog.get(), Event::kShowHelp);
 
-    std::cout << std::endl << "==== Invoke Button ====" << std::endl;
-    button->HandleHelp();
+    std::cout << std::endl << "==== Show help on Button ====" << std::endl;
+    button->HandleEvent(Event::kShowHelp);
 
-    std::cout << std::endl << "==== Invoke Dialog ====" << std::endl;
-    dialog->HandleHelp();
+    std::cout << std::endl << "==== Show help on Dialog ====" << std::endl;
+    dialog->HandleEvent(Event::kShowHelp);
 
-    std::cout << std::endl << "==== Invoke Application ====" << std::endl;
-    app->HandleHelp();
+    std::cout << std::endl << "==== Show help Application ====" << std::endl;
+    app->HandleEvent(Event::kShowHelp);
+
+    std::cout << std::endl;
+
+    std::cout << std::endl << "==== Print on Button ====" << std::endl;
+    button->HandleEvent(Event::kPrint);
+
+    std::cout << std::endl << "==== Print on Dialog ====" << std::endl;
+    dialog->HandleEvent(Event::kPrint);
+
+    std::cout << std::endl << "==== Print on Application ====" << std::endl;
+    app->HandleEvent(Event::kPrint);
 }
